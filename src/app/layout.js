d400271b4a87ab5236af09777b0e724fbc32911f
mikaeltv33n <mikaeltv33n>
2024-04-18 09:23:02 +0200

    post remove finalizing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import "./globals.css"
 import Link from "next/link"
-import { ToastContainer, Zoom } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
 
 export const metadata = {
   title: "Create Next App",
@@ -20,15 +18,6 @@ export default function RootLayout({ children }) {
           </nav>
         </header>
         {children}
-        <ToastContainer
-          position="bottom-center"
-          hideProgressBar={true}
-          draggable
-          closeButton={false}
-          stacked={true}
-          transition={Zoom}
-          autoClose={false}
-        />
       </body>
     </html>
   )
